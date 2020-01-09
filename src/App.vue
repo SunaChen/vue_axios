9332f9f532ab9b17546e85b40288df4ef37b7af7
陈素娜 <chensn@asiainfo.com>
2020-01-09 17:21:43 +0800

    接调用，订阅/发布消息

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,25 @@
 <template>
   <div class="container">
-    <section class="jumbotron">
-      <h3 class="jumbotron-heading">Search Github Users</h3>
-      <div>
-        <input type="text" placeholder="enter the name you search"/>
-        <button>Search</button>
-      </div>
-    </section>
-    <div class="row">
-      <div class="card">
-        <a href="https://github.com/reactjs" target="_blank">
-          <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style='width: 100px'/>
-        </a>
-        <p class="card-text">reactjs</p>
-      </div>
-      <div class="card">
-        <a href="https://github.com/reactjs" target="_blank">
-          <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style='width: 100px'/>
-        </a>
-        <p class="card-text">reactjs</p>
-      </div>
-      <div class="card">
-        <a href="https://github.com/reactjs" target="_blank">
-          <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style='width: 100px'/>
-        </a>
-        <p class="card-text">reactjs</p>
-      </div>
-      <div class="card">
-        <a href="https://github.com/reactjs" target="_blank">
-          <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style='width: 100px'/>
-        </a>
-        <p class="card-text">reactjs</p>
-      </div>
-      <div class="card">
-        <a href="https://github.com/reactjs" target="_blank">
-          <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style='width: 100px'/>
-        </a>
-        <p class="card-text">reactjs</p>
-      </div>
-    </div>
+    <Search></Search>
+    <user-Main></user-Main>
   </div>
 
 </template>
 
 <script>
+    import Search from './components/Search'
+    import Main from './components/Main'
     export default {
-        name: ""
+       data(){
+         return {
+
+
+         }
+       },
+      components:{
+         Search,
+         userMain:Main
+      }
     }
 </script>
 
